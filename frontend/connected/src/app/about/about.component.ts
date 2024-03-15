65547344b540a18d71c4ff7a5baa0c9d4d48ac52
N R <sneha.nr@dxc.com>
2024-03-15 16:29:42 +0530

    'about'

--- frontend/connected/src/app/about/about.component.ts
+++ frontend/connected/src/app/about/about.component.ts
@@ -8,26 +8,47 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AboutComponent {
   aboutForm: FormGroup;
+  experienceErrors: any;
+  skills = ['C++', 'C', 'C#', 'AngularJS', 'Java', 'UI/UX', '.net' , 'Presentation'];
 
   constructor(private formBuilder: FormBuilder) {
     this.aboutForm = this.formBuilder.group({
-      name: ['', [Validators.required]],
-      address: [''],
+      firstName: ['', [Validators.required]],
+      lastName: [''],
+      address: this.formBuilder.group({
+        city: [''],
+        state: [''],
+        country: ['']
+      }),
       gender: ['', [Validators.required]],
       mobile: ['', [Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]], // Assuming Indian mobile numbers
-      skill: [''],
+      skills: this.formBuilder.array([]),
       occupation: ['', [Validators.required]],
       education: ['', [Validators.required]],
-      experience: [''],
-      about: ['']
+      experience: ['',[Validators.required]],
+      about: ['', [Validators.required, this.aboutWordLimitValidator()]]
     });
   }
 
+  aboutWordLimitValidator() {
+    return (control: { value: { trim: () => { (): any; new(): any; split: { (arg0: RegExp): { (): any; new(): any; length: any; }; new(): any; }; }; }; }) => {
+      if (control.value) {
+        const words = control.value.trim().split(/\s+/).length;
+        if (words < 100 || words > 150) {
+          return { wordLimit: true };
+        }
+      }
+      return null;
+    };
+  }
+  
   onSubmit(): void {
     if (this.aboutForm.valid) {
       console.log('Form submitted successfully!');
       // Add your form submission logic here
-    }
+    } 
   }
+
 }
+
 
--- frontend/connected/src/app/app.module.ts
+++ frontend/connected/src/app/app.module.ts
@@ -8,11 +8,9 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 import { HeaderAuthComponent } from './header-auth/header-auth.component';
-<<<<<<< HEAD
 import { AboutComponent } from './about/about.component';
-=======
 import { OtpVerifyComponent } from './otp-verify/otp-verify.component';
->>>>>>> a98a9563cc2a1f7b4425652b505f07b120dc230b
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -22,11 +20,9 @@ import { OtpVerifyComponent } from './otp-verify/otp-verify.component';
     RegisterComponent,
     ForgetPasswordComponent,
     HeaderAuthComponent,
-<<<<<<< HEAD
-    AboutComponent
-=======
-    OtpVerifyComponent
->>>>>>> a98a9563cc2a1f7b4425652b505f07b120dc230b
+    AboutComponent,
+    OtpVerifyComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
