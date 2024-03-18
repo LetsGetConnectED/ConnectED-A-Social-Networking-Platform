8b4c11303e42fb25407559dc7cec5a20b829c101
Bhavishya Tomer <bhavishya.tomer@dxc.com>
2024-03-18 10:08:39 +0530

    registrion merge issues

--- frontend/connected/src/app/app-routing.module.ts
+++ frontend/connected/src/app/app-routing.module.ts
@@ -5,10 +5,7 @@ import { RegisterComponent } from './register/register.component';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 import { AboutComponent } from './about/about.component';
 import { OtpVerifyComponent } from './otp-verify/otp-verify.component';
-<<<<<<< HEAD
 import { DashboardComponent } from './dashboard/dashboard.component';
-=======
->>>>>>> bc3d5ddf15faf7b588449b45e7f31da96c3e5669
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to login page
@@ -16,10 +13,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   {path: 'forget-password',component:ForgetPasswordComponent},
   {path: 'about',component:AboutComponent},
-<<<<<<< HEAD
   {path: 'dashboard',component:DashboardComponent},
-=======
->>>>>>> bc3d5ddf15faf7b588449b45e7f31da96c3e5669
   {path:'otp',component:OtpVerifyComponent}
   // Add more routes here if needed
 ];
