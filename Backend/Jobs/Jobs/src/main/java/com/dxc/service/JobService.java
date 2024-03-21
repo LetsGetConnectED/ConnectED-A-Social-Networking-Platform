d0a465c75bdd36f5cb2ce25ee0fc74640db6522f
SriNandhini550 <srinandhini550@gmail.com>
2024-03-21 17:57:46 +0530

    Initial Commit

---
+++ Backend/Jobs/Jobs/src/main/java/com/dxc/service/JobService.java
@@ -0,0 +1,14 @@
+package com.dxc.service;
+
+import java.util.List;
+
+import com.dxc.model.Job;
+
+public class JobService {
+
+	public static List<Job> getRecommendedJobs(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
