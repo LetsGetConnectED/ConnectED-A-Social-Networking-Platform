dbc05e789e25217418e55fdb82fc762004c521fb
Bhavishya Tomer <bhavishyatomer@gmail.com>
2024-03-27 10:32:36 +0530

    dashboard

--- Backend/PostServices-3/PostServices-3/src/main/java/com/example/demo/Controller/PostController.java
+++ Backend/PostServices-3/PostServices-3/src/main/java/com/example/demo/Controller/PostController.java
@@ -10,15 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 //import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -29,6 +22,7 @@ import com.example.demo.Service.UserService;
 
  
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class PostController {
 
 	@Autowired
@@ -86,15 +80,6 @@ public class PostController {
 	            return new ResponseEntity<>("Failed to delete post with id " + id + " for user " + email, HttpStatus.NOT_FOUND);
 	        }
 	    }
-	 
-	 
-	 
-	 
-	 
-
-	 
-	 
-	 
 	 @GetMapping("/{email}/{postId}/image")
 	 public ResponseEntity<byte[]> getImageBytes(@PathVariable String email, @PathVariable int postId) {
 	     byte[] imageBytes = service.getImageBytes(email, postId);
