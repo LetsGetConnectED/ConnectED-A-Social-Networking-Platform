f28b10cf543aacf1832e27761850fc34c6a3dad0
SowndaryaMadhu <sownd0501@gmail.com>
2024-04-03 16:29:22 +0530

    PostService-User

--- Backend/PostServiceUser/src/main/java/com/connected/userpost/PostMicroServiceApplication.java
+++ Backend/PostServiceUser/src/main/java/com/connected/userpost/PostMicroServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.demo"})
+@ComponentScan(basePackages = {"com.connected.userpost"})
 
  
 public class PostMicroServiceApplication {
