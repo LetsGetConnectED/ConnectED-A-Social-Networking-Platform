c5c535264b421a7b3de181cd3b1916bdaf2b3454
Bhavishya Tomer <bhavishyatomer@gmail.com>
2024-04-04 20:47:17 +0530

    backend cleanup

--- Backend/ConnectED-Messaging/src/main/java/com/connected/messaging/chat/ChatMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.connected.messaging.chat;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    List<ChatMessage> findByChatId(String chatId);
-}
--- Backend/ConnectED-Messaging/src/main/java/com/connected/messaging/chatroom/ChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.connected.messaging.chatroom;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
-    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
-}
